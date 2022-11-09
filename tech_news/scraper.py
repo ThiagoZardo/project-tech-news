3a36009d79e08056a7d2cd4b7f4a88c2c5206c60
ThiagoZardo <thiago.artevisual@gmail.com>
2022-11-09 16:39:55 -0300

    Req 3 ok

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,14 +31,46 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css('a.next::attr(href)').get()
+    return url
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css('link[rel=canonical]::attr(href)').get()
+    title = selector.css('.entry-title::text').get().strip()
+    timestamp = selector.css('li.meta-date::text').get()
+    writer = selector.css('a.url.fn.n::text').get() or 0
+    tags = selector.css('.post-tags > ul > li > a::text').getall()
+    summary = selector.css(
+        '.entry-content > p:nth-of-type(1) *::text').getall()
+    comments_count = selector.css('.title-block::text')
+    category = selector.css('.label::text')
+
+    articles = {
+        'url': url,
+        'title': title,
+        'timestamp': timestamp,
+        'writer': writer,
+        'comments_count': comments_count,
+        'summary': "".join(summary).strip(),
+        'tags': tags,
+        'category': category,
+    }
+    return articles
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    quantity_news = 0
+    while quantity_news < amount:
+        url = 'https://blog.betrybe.com'
+        html_content = fetch(url)
+        list_news = scrape_novidades(html_content)
+        next_page = scrape_next_page_link(list_news)
+        newsletter = scrape_noticia(list_news)
+        fetch(next_page)
+        quantity_news += 1
+    return newsletter
