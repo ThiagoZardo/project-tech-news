eae3dc46fc6bd2e6f0f9baf250cf466a7cd47d62
ThiagoZardo <thiago.artevisual@gmail.com>
2022-11-09 23:42:32 -0300

    Req 07 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-from tech_news.database import (
-    search_news
-)
+from tech_news.database import(search_news)
 
 
 # Requisito 6
@@ -19,19 +17,16 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     try:
-        datetime.strptime(date, "%d-%m-%Y")
-
+        format_data = datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
         list_news = []
-        newlastter = search_news(
-            {'timestamp': {'$regex': '{data}'}}
-        )
+        newlastter = search_news({'timestamp': format_data})
         for new in newlastter:
             list_news.append((new['title'], new['url']))
 
         return list_news
-
     except:
         raise ValueError('Data inválida')
+
 
 
 # Requisito 8
@@ -44,7 +39,6 @@ def search_by_tag(tag):
         list_news.append((new['title'], new['url']))
 
     return list_news
-
 
 
 # Requisito 9
